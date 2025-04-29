aae9689a095797eb0c8d8475f10d2db97f660da0
Adam-Elmi <Adamcade123@gmail.com>
2025-04-29 16:02:36 +0300

    Fixed a minor error

--- src/scripts/currentSection.ts
+++ src/scripts/currentSection.ts
@@ -1,13 +1,21 @@
-// import { active_section } from "../../somdocs.config.json";
 (() => {
-  // const { bg_color, text_color, border, bold, italic, border_radius } =
-  //   active_section;
   const all_sections = document.querySelectorAll("#section");
   if (all_sections) {
-    all_sections.forEach((section) => {
+    all_sections.forEach((section, index) => {
       section.addEventListener("click", () => {
-        section.classList.add("active");
+        all_sections.forEach(sec => sec.classList.remove("active-section"));
+        sessionStorage.setItem("active-section", JSON.stringify(index));
+        update_active_section();
       })
     })
   }
+  function update_active_section() {
+    const get_active_section_index = sessionStorage.getItem("active-section") || 0;
+    if(get_active_section_index !== null || get_active_section_index !== "undefined") {
+      const parsedIndex = typeof get_active_section_index !== "number" ? parseInt(get_active_section_index) : get_active_section_index;
+      all_sections.forEach(section => section.classList.remove("active-section"));
+      all_sections[parsedIndex].classList.add("active-section");
+    }    
+  };
+  document.addEventListener("DOMContentLoaded", update_active_section);
 })();
