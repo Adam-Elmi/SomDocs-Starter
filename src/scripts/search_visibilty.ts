55fc8f02edc7f4f94f7520e53f369ce792cf676b
Adam-Elmi <Adamcade123@gmail.com>
2025-04-30 23:31:20 +0300

    Improved logic for controlling search visibility

--- src/scripts/search_visibilty.ts
+++ src/scripts/search_visibilty.ts
@@ -1,30 +1,33 @@
 (() => {
-  const search_btn = document.getElementById("search-btn");
-  const search_main_container = document.getElementById("search-main-container");
+  const search_btns = document.querySelectorAll("#search-btn");
+  const search_main_container = document.getElementById(
+    "search-main-container"
+  );
   const search_container = document.getElementById("search-container");
   const search_input = document.getElementById("search-input");
-  if (search_btn) {
-    search_btn.addEventListener("click", () => {
+  if (search_btns) {
+    search_btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
         (search_main_container as HTMLElement).style.display = "flex";
         search_input?.focus();
+      });
     });
   }
-  if(search_main_container) {
+  if (search_main_container) {
     search_main_container.addEventListener("click", () => {
-        (search_main_container as HTMLElement).style.display = "none";
-    })
-    
-      if(search_container) {
-        search_container.addEventListener("click", (e) => e.stopPropagation());
-      }
+      (search_main_container as HTMLElement).style.display = "none";
+    });
+
+    if (search_container) {
+      search_container.addEventListener("click", (e) => e.stopPropagation());
+    }
   }
 
   window.addEventListener("keydown", (e) => {
-    if(e.ctrlKey && e.key === "k") {
-        (search_main_container as HTMLElement).style.display = "flex";
-        e.preventDefault();
-        search_input?.focus();
+    if (e.ctrlKey && e.key === "k") {
+      (search_main_container as HTMLElement).style.display = "flex";
+      e.preventDefault();
+      search_input?.focus();
     }
-  })
-
+  });
 })();
