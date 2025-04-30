8eac662dc3629e8fc76e3113ff97c4d168196299
Adam-Elmi <Adamcade123@gmail.com>
2025-04-30 21:41:47 +0300

    Refined current section tracking logic

--- src/scripts/currentSection.ts
+++ src/scripts/currentSection.ts
@@ -1,21 +1,42 @@
+import { sections } from "../../somdocs.config.json";
 (() => {
+  const { contents } = sections;
   const all_sections = document.querySelectorAll("#section");
   if (all_sections) {
     all_sections.forEach((section, index) => {
       section.addEventListener("click", () => {
-        all_sections.forEach(sec => sec.classList.remove("active-section"));
+        all_sections.forEach((sec) => sec.classList.remove("active-section"));
         sessionStorage.setItem("active-section", JSON.stringify(index));
         update_active_section();
-      })
-    })
+        get_pathname_of_active_section();
+      });
+    });
   }
   function update_active_section() {
-    const get_active_section_index = sessionStorage.getItem("active-section") || 0;
-    if(get_active_section_index !== null || get_active_section_index !== "undefined") {
-      const parsedIndex = typeof get_active_section_index !== "number" ? parseInt(get_active_section_index) : get_active_section_index;
-      all_sections.forEach(section => section.classList.remove("active-section"));
-      all_sections[parsedIndex].classList.add("active-section");
-    }    
-  };
+    get_pathname_of_active_section();
+    const get_active_section_index =
+      sessionStorage.getItem("active-section") || 0;
+    if (
+      get_active_section_index !== null ||
+      get_active_section_index !== "undefined"
+    ) {
+      const parsedIndex =
+        typeof get_active_section_index !== "number"
+          ? parseInt(get_active_section_index)
+          : get_active_section_index;
+      all_sections.forEach((section) =>
+        section.classList.remove("active-section")
+      );
+      if(parsedIndex !== undefined) {
+        all_sections[parsedIndex].classList.add("active-section");
+      }
+    }
+  }
+  function get_pathname_of_active_section() {
+    const pathname = window.location.pathname;
+    const findIndex = contents.findIndex((section) => section.url === pathname);
+    sessionStorage.setItem("active-section", JSON.stringify(findIndex));
+    console.log(findIndex);
+  }
   document.addEventListener("DOMContentLoaded", update_active_section);
 })();
