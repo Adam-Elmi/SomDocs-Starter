b9f004758bd266a04aa64530fbc441db62aac529
Adam-Elmi <Adamcade123@gmail.com>
2025-04-30 23:31:46 +0300

    Added script to toggle sidebar visibility

---
+++ src/scripts/sidebar_visibilty.ts
@@ -0,0 +1,28 @@
+(() => {
+  const sidebar = document.getElementById("sidebar");
+  const close_btn = document.getElementById("close-btn");
+  const open_btn = document.getElementById("open-btn");
+  if (close_btn) {
+    close_btn.addEventListener("click", () => {
+      (sidebar as HTMLElement).style.display = "none";
+      console.log("Close");
+    });
+  }
+  if (open_btn) {
+    open_btn.addEventListener("click", () => {
+      (sidebar as HTMLElement).style.display = "flex";
+      console.log("Open");
+    });
+  }
+  window.addEventListener("resize", () => {
+    update_visiblity();
+  });
+  function update_visiblity() {
+    if (window.innerWidth >= 900) {
+      (sidebar as HTMLElement).style.display = "flex";
+    } else {
+      (sidebar as HTMLElement).style.display = "none";
+    }
+  }
+  update_visiblity();
+})();
