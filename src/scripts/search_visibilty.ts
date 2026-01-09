56931e419475f291dc7467d123e838f1c7bf97a1
Adam-Elmi <Adamcade123@gmail.com>
2026-01-09 21:59:26 +0300

    updated search visibility script

--- src/scripts/search_visibilty.ts
+++ src/scripts/search_visibilty.ts
@@ -1,37 +1,19 @@
 (() => {
   const search_btns = document.querySelectorAll("#search-btn");
-  const search_main_container = document.getElementById(
-    "search-main-container"
-  );
-  const search_container = document.getElementById("search-container");
-  const search_input = document.getElementById("search-input");
+
   if (search_btns) {
     search_btns.forEach((btn) => {
       btn.addEventListener("click", () => {
-        (search_main_container as HTMLElement).style.display = "flex";
-        search_input?.focus();
+        window.dispatchEvent(new CustomEvent("open-search"));
       });
     });
   }
-  if (search_main_container) {
-    search_main_container.addEventListener("click", () => {
-      (search_main_container as HTMLElement).style.display = "none";
-    });
-
-    if (search_container) {
-      search_container.addEventListener("click", (e) => e.stopPropagation());
-    }
-  }
 
   window.addEventListener("keydown", (e) => {
-    if (e.ctrlKey && e.key === "k") {
-      (search_main_container as HTMLElement).style.display = "flex";
+    if ((e.ctrlKey || e.metaKey) && e.key === "k") {
       e.preventDefault();
-      search_input?.focus();
-    }
-    if (e.key === "Escape") {
-      (search_main_container as HTMLElement).style.display = "none";  
-      e.preventDefault();
+      window.dispatchEvent(new CustomEvent("open-search"));
     }
   });
 })();
+
