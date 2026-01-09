485140e3d59436f66daf8c1a5af89cdba82345dc
Adam-Elmi <Adamcade123@gmail.com>
2026-01-09 21:59:25 +0300

    updated astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,47 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import mdx from "@astrojs/mdx";
+import { transformerMetaHighlight, transformerNotationDiff } from '@shikijs/transformers';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({ markdown: {
-   shikiConfig: {
-     theme: "github-dark-default",
-   }},
+export default defineConfig({
+  site: "https://somdocs.adam-elmi.vercel.app",
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+      transformers: [
+        transformerMetaHighlight(),
+        transformerNotationDiff(),
+        {
+          name: 'transformer-add-line-class',
+          preprocess(code) {
+            return code.replace(/\n$/, '');
+          },
+          code(node) {
+            if (node.tagName === 'pre') {
+              const codeEl = node.children.find(c => c.type === 'element' && c.tagName === 'code');
+              if (codeEl && codeEl.hasOwnProperty('children')) {
+                codeEl.children = codeEl.children.filter(c => c.type !== 'text' || !/^\s*$/.test(c.value));
+
+                const children = codeEl.children;
+                if (!children) return;
+                children.forEach(lineNode => {
+                  if (lineNode.type === 'element') {
+                    const classes = new Set(lineNode.properties.class || []);
+                    classes.add('line');
+                    lineNode.properties.class = Array.from(classes);
+                  }
+                })
+              }
+            }
+          }
+        }
+      ],
+    },
+  },
   devToolbar: {
     enabled: false,
   },
@@ -22,5 +55,5 @@ export default defineConfig({ markdown: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
